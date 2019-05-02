f442ef2c9799965d35b68a51d219c9f9d4c8ec11
Isaac Tretta <isaac.tretta@ucr.ac.cr>
2019-05-02 15:16:06 -0600

    Añadido en el Home/About el nombre del equipo y los integrante, eliminacion del link de creación de secciones

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/SeccionController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/SeccionController.cs
@@ -20,100 +20,100 @@ namespace Opiniometro_WebApp.Controllers
             return View(db.Seccions.ToList());
         }
 
-        // GET: Seccion/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Seccion seccion = db.Seccions.Find(id);
-            if (seccion == null)
-            {
-                return HttpNotFound();
-            }
-            return View(seccion);
-        }
+        //// GET: Seccion/Details/5
+        //public ActionResult Details(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Seccion seccion = db.Seccions.Find(id);
+        //    if (seccion == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(seccion);
+        //}
 
-        // GET: Seccion/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //// GET: Seccion/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: Seccion/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Titulo")] Seccion seccion)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Seccions.Add(seccion);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        //// POST: Seccion/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Titulo")] Seccion seccion)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Seccions.Add(seccion);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
 
-            return View(seccion);
-        }
+        //    return View(seccion);
+        //}
 
-        // GET: Seccion/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Seccion seccion = db.Seccions.Find(id);
-            if (seccion == null)
-            {
-                return HttpNotFound();
-            }
-            return View(seccion);
-        }
+        //// GET: Seccion/Edit/5
+        //public ActionResult Edit(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Seccion seccion = db.Seccions.Find(id);
+        //    if (seccion == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(seccion);
+        //}
 
-        // POST: Seccion/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Titulo")] Seccion seccion)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(seccion).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(seccion);
-        }
+        //// POST: Seccion/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Titulo")] Seccion seccion)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(seccion).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(seccion);
+        //}
 
-        // GET: Seccion/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Seccion seccion = db.Seccions.Find(id);
-            if (seccion == null)
-            {
-                return HttpNotFound();
-            }
-            return View(seccion);
-        }
+        //// GET: Seccion/Delete/5
+        //public ActionResult Delete(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Seccion seccion = db.Seccions.Find(id);
+        //    if (seccion == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(seccion);
+        //}
 
-        // POST: Seccion/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            Seccion seccion = db.Seccions.Find(id);
-            db.Seccions.Remove(seccion);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: Seccion/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(string id)
+        //{
+        //    Seccion seccion = db.Seccions.Find(id);
+        //    db.Seccions.Remove(seccion);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
         protected override void Dispose(bool disposing)
         {
