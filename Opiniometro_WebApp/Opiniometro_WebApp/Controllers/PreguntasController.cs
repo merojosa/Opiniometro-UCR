9cb76c3f60eacdd34e32b1c4c39754b4245b626e
Fabian Antonio Rojas Masis <fabian.rojasmasis@ucr.ac.cr>
2019-05-01 20:27:25 -0600

    Creacion del controlador de personas y las vistas.

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/PreguntasController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/PreguntasController.cs
@@ -1,30 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Opiniometro_WebApp.Models;
 
 namespace Opiniometro_WebApp.Controllers
 {
-    public class PreguntasCotroller : Controller
+    public class PreguntasController : Controller
     {
+        private Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
+
+        // GET: Preguntas
         public ActionResult Index()
         {
+            return View(db.Preguntas.ToList());
+        }
+
+        // GET: Preguntas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Preguntas preguntas = db.Preguntas.Find(id);
+            if (preguntas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preguntas);
+        }
+
+        // GET: Preguntas/Create
+        public ActionResult Create()
+        {
             return View();
         }
 
-        public ActionResult About()
+        // POST: Preguntas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Planteamiento")] Preguntas preguntas)
         {
-            ViewBag.Message = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                db.Preguntas.Add(preguntas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-            return View();
+            return View(preguntas);
         }
 
-        public ActionResult Contact()
+        // GET: Preguntas/Edit/5
+        public ActionResult Edit(string id)
         {
-            ViewBag.Message = "Your contact page.";
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Preguntas preguntas = db.Preguntas.Find(id);
+            if (preguntas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preguntas);
+        }
 
-            return View();
+        // POST: Preguntas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Planteamiento")] Preguntas preguntas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(preguntas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(preguntas);
+        }
+
+        // GET: Preguntas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Preguntas preguntas = db.Preguntas.Find(id);
+            if (preguntas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preguntas);
+        }
+
+        // POST: Preguntas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Preguntas preguntas = db.Preguntas.Find(id);
+            db.Preguntas.Remove(preguntas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
