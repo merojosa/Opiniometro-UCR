f73927833b8e8d5bb4b40b333f5f29a995db35c8
JoseAndresMejiasRojas <jose.mejiasrojas@ucr.ac.cr>
2019-05-10 16:22:22 -0600

    Borradas las tablas creadas para el sprint 0.  Se creó la tabla Usuario.  Se programó el procedimiento almacenado para insertar un Usuario y generar una contraseña encriptada a partir de la brindada.  Se creó una función almacenada para hacer login, la cual devuelve 1 en caso de que sí pueda entrar, 0 si no.

--- Opiniometro_Datos/Opiniometro_Datos/Curso.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Curso]
-(
-	[CodigoCurso] NVARCHAR(50) NOT NULL PRIMARY KEY, 
-    [NombreCurso] NVARCHAR(50) NOT NULL
-)
---
+++ Opiniometro_Datos/Opiniometro_Datos/Script.PostDeployment.sql
@@ -0,0 +1,50 @@
+﻿EXEC sp_MSForEachTable 'DISABLE TRIGGER ALL ON ?'
+GO
+EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'
+GO
+EXEC sp_MSForEachTable 'DELETE FROM ?'
+GO
+EXEC sp_MSForEachTable 'ALTER TABLE ? CHECK CONSTRAINT ALL'
+GO
+EXEC sp_MSForEachTable 'ENABLE TRIGGER ALL ON ?'
+
+
+-- PROCEDURES
+IF OBJECT_ID('SP_AgregarUsuario') IS NOT NULL
+	DROP PROCEDURE SP_AgregarUsuario
+GO
+CREATE PROCEDURE SP_AgregarUsuario
+	@Correo			NVARCHAR(50),
+	@Contrasenna	NVARCHAR(50),
+	@Cedula			CHAR(9)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @Id UNIQUEIDENTIFIER=NEWID()
+
+	INSERT INTO Usuario
+	VALUES (@Correo, HASHBYTES('SHA2_512', @Contrasenna+CAST(@Id AS NVARCHAR(36))), 1, @Cedula, @Id)
+END
+GO
+
+-- FUNCTIONS
+IF OBJECT_ID('SF_LoginUsuario') IS NOT NULL
+	DROP FUNCTION SF_LoginUsuario
+GO
+CREATE FUNCTION SF_LoginUsuario
+	(@Correo NVARCHAR(50), @Contrasenna NVARCHAR(50))
+RETURNS BIT
+BEGIN
+	DECLARE @CorreoBuscar NVARCHAR(50), @Result BIT
+
+	-- Buscar que el correo y contrasenna calcen con lo que hay en la tabla Usuario.
+	SET @CorreoBuscar =	(SELECT CorreoInstitucional 
+						FROM Usuario
+						WHERE CorreoInstitucional=@Correo AND Contrasena=HASHBYTES('SHA2_512', @Contrasenna+CAST(Id AS NVARCHAR(36))))
+	IF(@CorreoBuscar IS NULL)	-- Si no calzan, no hay autenticacion.
+		SET @Result = 0
+	ELSE						-- Si hay autenticacion
+		SET @Result = 1
+	RETURN @Result
+END;
+	
