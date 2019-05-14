d700e9773dbd8c08875a4248700a1490cbeb51f6
joseandresmr <jose.mejiasrojas@ucr.ac.cr>
2019-05-13 21:34:14 -0600

    Migración a nueva solución con la opción de Individual User Accounts.

---
+++ Opiniometro_WebApp/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Entity.Core.Objects;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Opiniometro_WebApp.Models;
+
+namespace Opiniometro_WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Action method que recibe un correo y contraseña por medio de un FormCollection.
+        [HttpPost]
+        public string Login(FormCollection form_collection)
+        {
+            ObjectParameter exito = new ObjectParameter("Resultado", 0);
+            db.SP_LoginUsuario(form_collection["Correo"], form_collection["Contrasenna"], exito);
+
+            if ((bool)exito.Value == true)
+                return "Login exitoso";
+            else
+                return "Login fallido";
+        }
+    }
+}
