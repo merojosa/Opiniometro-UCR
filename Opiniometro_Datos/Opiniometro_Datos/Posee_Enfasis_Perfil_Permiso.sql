b8e61e7aa6adae6b9bce22febd8d6511e50f2948
Daniel_Ramirez <daniel.ramirezumana@ucr.ac.cr>
2019-05-16 15:21:13 -0600

    Tablas creadas (Empadronado y Posee_Enfasis_Perfil_Permiso)

---
+++ Opiniometro_Datos/Opiniometro_Datos/Posee_Enfasis_Perfil_Permiso.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Posee_Enfasis_Perfil_Permiso]
+(
+	[NumeroEnfasis]	TINYINT			NOT NULL,
+	[SiglaCarrera]	NVARCHAR(10)	NOT NULL,
+	[IdPerfil]		VARCHAR(10)		NOT NULL,
+	[IdPermiso]		TINYINT			NOT NULL,
+	CONSTRAINT PK_PoseeEnfasisPerfil
+		PRIMARY KEY (NumeroEnfasis, SiglaCarrera, IdPerfil, IdPermiso),
+	CONSTRAINT FK_PoseeEnfasisPerfil_Perfil
+		FOREIGN KEY (IdPerfil) REFERENCES Perfil(Id)
+			ON DELETE NO ACTION
+			ON UPDATE CASCADE,
+	CONSTRAINT FK_PoseeEnfasisPerfil_Permiso
+		FOREIGN KEY (IdPermiso) REFERENCES Permiso(Id)
+			ON DELETE NO ACTION
+			ON UPDATE CASCADE/*,
+			--Lo siguiente tiene dependencias:
+	CONSTRAINT FK_PoseeEnfasisPerfil_Enfasis
+		FOREIGN KEY (NumeroEnfasis, SiglaCarrera) REFERENCES Enfasis(Numero, SiglaCarrera)
+			ON DELETE NO ACTION
+			ON UPDATE CASCADE*/
+)
