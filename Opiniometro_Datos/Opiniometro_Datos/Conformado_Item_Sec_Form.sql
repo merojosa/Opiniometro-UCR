dd392288f8555df26176ca35fd841830b08a82a7
Felipe Carmona <luis.carmonacastro@ucr.ac.cr>
2019-05-17 14:08:19 -0600

    Bases listas para subir a master

--- Opiniometro_Datos/Opiniometro_Datos/Conformado_Item_Sec_Form.sql
+++ Opiniometro_Datos/Opiniometro_Datos/Conformado_Item_Sec_Form.sql
@@ -5,9 +5,12 @@
 	TituloSeccion NVARCHAR(120) NOT NULL, 
 	NombreFormulario VARCHAR(30),
 	CONSTRAINT PK_Conformado_Item_Sec_Form PRIMARY KEY (ItemId,CodigoFormulario,TituloSeccion),
-	CONSTRAINT FK_Con_Ite FOREIGN KEY(ItemID) REFERENCES Item(ItemId),
-	CONSTRAINT FK_Con_For FOREIGN KEY(CodigoFormulario) REFERENCES Formulario(CodigoFormulario),
+	CONSTRAINT FK_Con_Ite FOREIGN KEY(ItemID) REFERENCES Item(ItemId)
+		ON UPDATE CASCADE,
+	CONSTRAINT FK_Con_For FOREIGN KEY(CodigoFormulario) REFERENCES Formulario(CodigoFormulario)
+		ON UPDATE CASCADE,
 	CONSTRAINT FK_Con_Sec FOREIGN KEY(TituloSeccion) REFERENCES Seccion(Titulo)
+		ON UPDATE CASCADE
 	--Orden_Item 
 	--Orden_Seccion
 )
