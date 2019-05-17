6da8f48ad52a8737bcb3ae2cf22d9634fa96ce83
Felipe Carmona <luis.carmonacastro@ucr.ac.cr>
2019-05-17 14:19:32 -0600

    Cambio de FK de Conformado

--- Opiniometro_Datos/Opiniometro_Datos/Conformado_Item_Sec_Form.sql
+++ Opiniometro_Datos/Opiniometro_Datos/Conformado_Item_Sec_Form.sql
@@ -5,7 +5,7 @@
 	TituloSeccion NVARCHAR(120) NOT NULL, 
 	NombreFormulario VARCHAR(30),
 	CONSTRAINT PK_Conformado_Item_Sec_Form PRIMARY KEY (ItemId,CodigoFormulario,TituloSeccion),
-	CONSTRAINT FK_Con_Ite FOREIGN KEY(ItemID) REFERENCES Item(ItemId)
+	CONSTRAINT FK_Con_Ite FOREIGN KEY(ItemId) REFERENCES Item(ItemId)
 		ON UPDATE CASCADE,
 	CONSTRAINT FK_Con_For FOREIGN KEY(CodigoFormulario) REFERENCES Formulario(CodigoFormulario)
 		ON UPDATE CASCADE,
