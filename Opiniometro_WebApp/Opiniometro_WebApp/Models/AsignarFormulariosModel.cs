74777eefea8178ba3a41232faa948c1b31cf1dc6
hernan torres <hernan.torres@ucr.ac.cr>
2019-05-17 14:54:40 -0600

    Primer set up de la vistra de asignar formularios, el modelo solo contiene una tabla.

---
+++ Opiniometro_WebApp/Opiniometro_WebApp/Models/AsignarFormulariosModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Opiniometro_WebApp.Models
+{
+    public class AsignarFormularios
+    {
+        public IEnumerable<SelectListItem> Cursos { get; set; }
+    }
+}
