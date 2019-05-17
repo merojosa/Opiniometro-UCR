9c20eb2f9dfaa27d47f5c2a58cef592681a9bff9
Frankin Ramírez <frank221097@gmail.com>
2019-05-17 09:46:50 -0600

    Creacion del controlador Formulario_Respuesta.cs (no generado automcaticamente)

---
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/Formulario_Respuesta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Opiniometro_WebApp.Controllers
+{
+    public class Formulario_Respuesta : System.Web.Mvc.Controller
+    {
+    }
+}
