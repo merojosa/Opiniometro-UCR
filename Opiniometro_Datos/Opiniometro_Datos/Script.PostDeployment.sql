9539c5617e5dd9af495176d90d01af5b9b27066a
Isaac Tretta <isaac.tretta@ucr.ac.cr>
2019-05-18 22:55:17 -0600

    Agregado de información para pruebas en el script

--- Opiniometro_Datos/Opiniometro_Datos/Script.PostDeployment.sql
+++ Opiniometro_Datos/Opiniometro_Datos/Script.PostDeployment.sql
@@ -58,13 +58,75 @@ VALUES	('116720500', 'Jose Andrés', 'Mejías', 'Rojas', 'Desamparados de Alajue
 		('115003456', 'Daniel', 'Escalante', 'Perez', 'Desamparados de San José.'),
 		('117720910', 'Jose Andrés', 'Mejías', 'Rojas', 'La Fortuna de San Carlos.'),
 		('236724501', 'Jose Andrés', 'Mejías', 'Rojas', 'Sarchí, Alajuela.'),
-		('100000001', 'CX', 'Solutions', 'S.A.', 'San Pedro Montes de Oca');
+		--Agregado de datos para visualizacion a cargo de CX Solutions
+		('100000001', 'CX', 'Solutions', 'S.A.', 'San Pedro Montes de Oca'),
+		('100000002', 'Marta', 'Rojas', 'Sanches', '300 metros norte de Pulmitan'),--Profesora
+		--Estudiantes
+		('100000003', 'Juan', 'Briceño', 'Lupon', '400 metros norte del Heraldo de la Grieta'),
+		('100000005', 'Pepito', 'Fonsi', 'Monge', '20 metros norte del Blue del lado Rojo'),
+		('100000004', 'Maria', 'Fallas', 'Merdi', 'Costado este del estandarte de top');
 
 EXEC SP_AgregarUsuario @Correo='jose.mejiasrojas@ucr.ac.cr', @Contrasenna='123456', @Cedula='116720500'
 EXEC SP_AgregarUsuario @Correo='daniel.escalanteperez@ucr.ac.cr', @Contrasenna='Danielito', @Cedula='115003456'
 EXEC SP_AgregarUsuario @Correo='rodrigo.cascantejuarez@ucr.ac.cr', @Contrasenna='contrasena', @Cedula='117720910'
 EXEC SP_AgregarUsuario @Correo='luis.quesadaborbon@ucr.ac.cr', @Contrasenna='LigaDeportivaAlajuelense', @Cedula='236724501'
 
+--Script C.X. Solutions
+
+--Item
+INSERT INTO Item(ItemId, TextoPregunta, Categoria, TieneObservacion, TipoPregunta)
+VALUES  (1500, '¿El profesor repuso clases cuando fue necesario?', 'Curso', 1, 3),
+		(1300, '¿El profesor entrego la carta del estudiante en las fechas indicadas por el reglamento?', 'Responsabilidades', 1, 3),
+		(2100, '¿Que opina del curso?', 'Opinion', 0, 1),
+		(2500, '¿Que opina del profesor?', 'Opinion', 0, 1);
+
+--Item-Texto Libre
+INSERT INTO Texto_Libre (ItemId)
+VALUES  (2100),
+		(2500);
+
+--Item-Si/no
+INSERT INTO Seleccion_Unica (ItemId, IsaLikeDislike)
+VALUES  (1500, 1),
+		(1300, 1);
+
+--Seccion
+INSERT INTO Seccion (Titulo, Descripcion)
+VALUES  ('Evaluación de aspectos reglamentarios del profesor', 'Conteste a las preguntas relacionadas a aspectos reglamentarios que el profesor debe cumplir.'),
+		('Opinion general del curso', 'Describa las opiniones que le han generado el profesor con respecto al curso tratado.');
+
+--Formulario
+INSERT INTO Formulario (CodigoFormulario, Nombre)
+VALUES  ('131313', 'Evaluación de Profesores de la ECCI');
+
+--Profesor
+INSERT INTO Profesor (CedulaProfesor)
+VALUES  ('100000002');
+
+--Formulario Respuesta
+INSERT INTO Formulario_Respuesta (Fecha, CodigoFormulario, CedulaPersona, CedulaProfesor, AnnoGrupo, SemestreGrupo, NumeroGrupo, SiglaGrupo, Completado)
+VALUES  ('2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', 1),
+		('2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', 1),
+		('2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', 1);
+
+--Responde
+INSERT INTO Responde (ItemId, TituloSeccion, FechaRespuesta, CodigoFormularioResp, CedulaPersona, CedulaProfesor, AnnoGrupoResp, SemestreGrupoResp, NumeroGrupoResp, SiglaGrupoResp, Observacion, Respuesta)
+VALUES  (1500, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', 'Nunca tuvimos que reponer clases', '3'),
+		(1300, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', 'La profesora olvido enviar la carta del estudiante pero si la revisamos en la primera semana de clases', '2'),
+		(2100, 'Opinion general del curso', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', '', 'La materia estuvo muy interesante y espero poder aplicarla en el futuro en el trabajo'),
+		(2500, 'Opinion general del curso', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', '', 'La profesora tardo mucho para devolver las evaluaciones'),
+		--Segunda evaluacion
+		(1500, 'Evaluación de aspectos reglamentarios del profesor', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', 'No fue necesario reponer clases', '2'),
+		(1300, 'Evaluación de aspectos reglamentarios del profesor', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', 'Revisamos la carta del estudiante en la primera semana', '1'),
+		(2100, 'Opinion general del curso', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', '', 'No estoy seguro de si en el ambiente laboral me servira la materia'),
+		(2500, 'Opinion general del curso', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', '', 'La profesora logro que las clases fueran muy entretenidas y dinámicas'),
+		--Tercera evaluacion
+		(1500, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', 'Me repuso una clase a la que falte', '1'),
+		(1300, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', '1'),
+		(2100, 'Opinion general del curso', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', 'Entretenido'),
+		(2500, 'Opinion general del curso', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', 'Muy buena profesora');
+
+--Fin Por el momento de script CX Solutions
 
 --Script JJAPH
 
@@ -116,11 +178,13 @@ INSERT INTO Curso (Sigla, Nombre, Tipo,CodigoUnidad)
 VALUES ('DE2001', 'PRINCIPIOS DEL DERECHO PRIVADO I', 1,'UC-485648')
 
 ----Grupos
---INSERT INTO Grupo(Numero, SiglaCurso, Anno, Semestre)
---VALUES ('CI1330', 1, 2019, 1)
 
---INSERT INTO Grupo(Numero, SiglaCurso, Anno, Semestre)
---VALUES ('CI1330', 2, 2019, 1)
+--Agregado de datos para visualizacion a cargo de CX Solutions
+INSERT INTO Grupo(SiglaCurso, Numero, AnnoGrupo, SemestreGrupo)
+VALUES ('CI1330', 1, 2017, 2)
+
+INSERT INTO Grupo(SiglaCurso, Numero, AnnoGrupo, SemestreGrupo)
+VALUES ('CI1330', 2, 2018, 1)
 
 --INSERT INTO Grupo(Numero, SiglaCurso, Anno, Semestre)
 --VALUES ('CI1331', 1, 2019, 1)
