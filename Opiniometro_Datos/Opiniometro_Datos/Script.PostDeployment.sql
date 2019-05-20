064b2584eb19c0b23320eb3098d943ad31f4cace
Isaac Tretta <isaac.tretta@ucr.ac.cr>
2019-05-19 22:20:14 -0600

    Script Deployment de C.X. Funcional.

--- Opiniometro_Datos/Opiniometro_Datos/Script.PostDeployment.sql
+++ Opiniometro_Datos/Opiniometro_Datos/Script.PostDeployment.sql
@@ -166,21 +166,24 @@ INSERT INTO Grupo(SiglaCurso, Numero, AnnoGrupo, SemestreGrupo)
 VALUES ('CI1330', 1, 2017, 2);
 
 --Item
-INSERT INTO Item(ItemId, TextoPregunta, Categoria, TieneObservacion, TipoPregunta)
-VALUES  (1300, '¿El profesor repuso clases cuando fue necesario?', 'Curso', 1, 3),
-		(1500, '¿El profesor entrego la carta del estudiante en las fechas indicadas por el reglamento?', 'Responsabilidades', 1, 3),
-		(2500, '¿Que opina del curso?', 'Opinion', 0, 1),
-		(2100, '¿Que opina del profesor?', 'Opinion', 0, 1);
+DELETE FROM Item;
+DBCC CHECKIDENT ('Item', RESEED, 0);
+
+INSERT INTO Item(TextoPregunta, Categoria, TieneObservacion, TipoPregunta)
+VALUES  ('¿El profesor repuso clases cuando fue necesario?', 'Curso', 1, 3),
+		('¿El profesor entrego la carta del estudiante en las fechas indicadas por el reglamento?', 'Responsabilidades', 1, 3),
+		('¿Que opina del curso?', 'Opinion', 0, 1),
+		('¿Que opina del profesor?', 'Opinion', 0, 1);
 
 --Item-Texto Libre
 INSERT INTO Texto_Libre (ItemId)
-VALUES  (2500),
-		(2100);
+VALUES  (3),
+		(4);
 
 --Item-Si/no
 INSERT INTO Seleccion_Unica (ItemId, IsaLikeDislike)
-VALUES  (1300, 1),
-		(1500, 1);
+VALUES  (1, 1),
+		(2, 1);
 
 --Seccion
 INSERT INTO Seccion (Titulo, Descripcion)
@@ -203,19 +206,19 @@ VALUES  ('2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', 1
 
 --Responde
 INSERT INTO Responde (ItemId, TituloSeccion, FechaRespuesta, CodigoFormularioResp, CedulaPersona, CedulaProfesor, AnnoGrupoResp, SemestreGrupoResp, NumeroGrupoResp, SiglaGrupoResp, Observacion, Respuesta)
-VALUES  (1300, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', 'Nunca tuvimos que reponer clases', '3'),
-		(1500, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', 'La profesora olvido enviar la carta del estudiante pero si la revisamos en la primera semana de clases', '2'),
-		(2500, 'Opinion general del curso', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', '', 'La materia estuvo muy interesante y espero poder aplicarla en el futuro en el trabajo'),
-		(2100, 'Opinion general del curso', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', '', 'La profesora tardo mucho para devolver las evaluaciones'),
+VALUES  (1, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', 'Nunca tuvimos que reponer clases', '3'),
+		(2, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', 'La profesora olvido enviar la carta del estudiante pero si la revisamos en la primera semana de clases', '2'),
+		(3, 'Opinion general del curso', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', '', 'La materia estuvo muy interesante y espero poder aplicarla en el futuro en el trabajo'),
+		(4, 'Opinion general del curso', '2017-4-5', '131313', '100000003', '100000002', 2017, 2, 1, 'CI1330', '', 'La profesora tardo mucho para devolver las evaluaciones'),
 		--Segunda evaluacion
-		(1300, 'Evaluación de aspectos reglamentarios del profesor', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', 'No fue necesario reponer clases', '2'),
-		(1500, 'Evaluación de aspectos reglamentarios del profesor', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', 'Revisamos la carta del estudiante en la primera semana', '1'),
-		(2500, 'Opinion general del curso', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', '', 'No estoy seguro de si en el ambiente laboral me servira la materia'),
-		(2100, 'Opinion general del curso', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', '', 'La profesora logro que las clases fueran muy entretenidas y dinámicas'),
+		(1, 'Evaluación de aspectos reglamentarios del profesor', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', 'No fue necesario reponer clases', '2'),
+		(2, 'Evaluación de aspectos reglamentarios del profesor', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', 'Revisamos la carta del estudiante en la primera semana', '1'),
+		(3, 'Opinion general del curso', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', '', 'No estoy seguro de si en el ambiente laboral me servira la materia'),
+		(4, 'Opinion general del curso', '2017-3-6', '131313', '100000004', '100000002', 2017, 2, 1, 'CI1330', '', 'La profesora logro que las clases fueran muy entretenidas y dinámicas'),
 		--Tercera evaluacion
-		(1300, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', 'Me repuso una clase a la que falte', '1'),
-		(1500, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', '1'),
-		(2500, 'Opinion general del curso', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', 'Entretenido'),
-		(2100, 'Opinion general del curso', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', 'Muy buena profesora');
+		(1, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', 'Me repuso una clase a la que falte', '1'),
+		(2, 'Evaluación de aspectos reglamentarios del profesor', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', '1'),
+		(3, 'Opinion general del curso', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', 'Entretenido'),
+		(4, 'Opinion general del curso', '2017-4-18', '131313', '100000005', '100000002', 2017, 2, 1, 'CI1330', '', 'Muy buena profesora');
 
 --Fin Por el momento de script CX Solutions
