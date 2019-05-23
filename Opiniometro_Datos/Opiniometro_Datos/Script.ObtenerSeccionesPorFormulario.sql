76d3e508b5a73860991f2165d57f7e2160b7cb46
Frankin Ramírez <frank221097@gmail.com>
2019-05-23 10:55:42 -0600

    Se cambio el proceso almacenado Secciones por Formulario por un Script

---
+++ Opiniometro_Datos/Opiniometro_Datos/Script.ObtenerSeccionesPorFormulario.sql
@@ -0,0 +1,22 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+CREATE PROCEDURE [dbo].[Obtener_Secciones_Por_Formulario]
+	@ForCod char(6)
+AS
+BEGIN
+	SET NOCOUNT ON
+	SELECT C.TituloSeccion
+	FROM Conformado_Item_Sec_Form C
+	WHERE C.CodigoFormulario = @ForCod
+END
+
