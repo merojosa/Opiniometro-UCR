fe7eb2000cc66072e5b58add50225c545853171d
JoseAndresMejiasRojas <jose.mejiasrojas@ucr.ac.cr>
2019-05-22 22:36:13 -0600

    Arreglo mayores de configuración con la base y la web app para evitar conflictos.  Se rehizo el controlador Seleccion_Unica, se arregló el problema con [ValidateAntiForgeryToken], se agregaron tablas al modelo.

--- Opiniometro_WebApp/Opiniometro_WebApp/Models/OpiniometroModel.Context.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Models/OpiniometroModel.Context.cs
@@ -48,6 +48,7 @@ namespace Opiniometro_WebApp.Models
         public virtual DbSet<Permiso> Permiso { get; set; }
         public virtual DbSet<Persona> Persona { get; set; }
         public virtual DbSet<Posee_Enfasis_Perfil_Permiso> Posee_Enfasis_Perfil_Permiso { get; set; }
+        public virtual DbSet<Preguntas> Preguntas { get; set; }
         public virtual DbSet<Profesor> Profesor { get; set; }
         public virtual DbSet<Responde> Responde { get; set; }
         public virtual DbSet<Seccion> Seccion { get; set; }
@@ -57,6 +58,29 @@ namespace Opiniometro_WebApp.Models
         public virtual DbSet<Tiene_Grupo_Formulario> Tiene_Grupo_Formulario { get; set; }
         public virtual DbSet<Unidad_Academica> Unidad_Academica { get; set; }
         public virtual DbSet<Usuario> Usuario { get; set; }
+    
+        public virtual ObjectResult<DatosEstudiante_Result> DatosEstudiante(string cedula)
+        {
+            var cedulaParameter = cedula != null ?
+                new ObjectParameter("Cedula", cedula) :
+                new ObjectParameter("Cedula", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<DatosEstudiante_Result>("DatosEstudiante", cedulaParameter);
+        }
+    
+        public virtual ObjectResult<MostrarEstudiantes_Result> MostrarEstudiantes()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<MostrarEstudiantes_Result>("MostrarEstudiantes");
+        }
+    
+        public virtual ObjectResult<string> NombrePersona(string cedula)
+        {
+            var cedulaParameter = cedula != null ?
+                new ObjectParameter("Cedula", cedula) :
+                new ObjectParameter("Cedula", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<string>("NombrePersona", cedulaParameter);
+        }
     
         public virtual int SP_AgregarUsuario(string correo, string contrasenna, string cedula)
         {
