e54a66c8c543678046220973f92d99a5a6f053f6
b22539 <b22539@ECCI393310W4-17>
2019-05-23 14:04:26 -0600

    Agregada ventanas de Creacion de Usuario y de ver personas en la base para el administrador con su respectivo controlador.

---
+++ Opiniometro_Datos/Opiniometro_Datos/Script.FiltrarUsuarios.sql
@@ -0,0 +1,27 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+-- Ver los usuarios agregados recientemente
+IF OBJECT_ID('SP_FiltrarUsuarios') IS NOT NULL
+	DROP PROCEDURE SP_FiltrarUsuarios
+GO
+CREATE PROCEDURE SP_FiltrarUsuarios
+@Ced VARCHAR(10)
+AS
+BEGIN
+	SELECT Cedula, Nombre, Apellido1, Apellido2
+	FROM Persona
+	WHERE Cedula LIKE @Ced+'%';
+END
+GO
+
+--EXEC SP_FiltrarUsuarios @Ced = '12';
---
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity.Core.Objects;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Opiniometro_WebApp.Models;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+
+namespace Opiniometro_WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
+
+        public ActionResult VerPersonas()
+        {
+            
+            return View();
+        }
+    }
+}
