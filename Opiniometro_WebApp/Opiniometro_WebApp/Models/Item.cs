878ecb25e93f91d92029fe77b8ff8aab2ef44d78
Frankin Ramírez <frank221097@gmail.com>
2019-06-06 14:27:28 -0600

    Se implementa las condiciones de mostrar un grafico, al menos que sea una pregunta tipo texto, y solo poder ver observaciones si existen observaciones para una pregunta en especifico

--- Opiniometro_WebApp/Opiniometro_WebApp/Models/Item.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Models/Item.cs
@@ -27,7 +27,6 @@ namespace Opiniometro_WebApp.Models
         public Nullable<bool> TieneObservacion { get; set; }
         public byte TipoPregunta { get; set; }
     
-        public virtual Responde responde_respuesta { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Conformado_Item_Sec_Form> Conformado_Item_Sec_Form { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
