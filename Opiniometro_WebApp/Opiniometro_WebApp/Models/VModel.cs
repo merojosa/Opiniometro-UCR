4af4bfac0efe18749c6326771489232028fa6a86
Frankin Ramírez <frank221097@gmail.com>
2019-06-07 09:30:53 -0600

    .

---
+++ Opiniometro_WebApp/Opiniometro_WebApp/Models/VModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web;
+
+namespace Opiniometro_WebApp.Models
+{
+    public class VModel
+    {
+        public IEnumerable<SelectListItem> Profesores { get; set; }
+    }
+}
