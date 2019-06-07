83e158da2c7da74e36d9197ba9f30be5ebc39aa3
Felipe Almanza <f.almanza4@gmail.com>
2019-06-07 12:03:31 -0600

    Se inicia con la Seleccion de permisos.

---
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/SeleccionPermisosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Opiniometro_WebApp.Models;
+
+namespace Opiniometro_WebApp.Controllers
+{
+    public class SeleccionPermisosController : Controller
+    {
+        private Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
+        // GET: SeleccionPermisos
+        public ActionResult SeleccionPermisosView()
+        {
+            return View();
+        }
+    }
+}
