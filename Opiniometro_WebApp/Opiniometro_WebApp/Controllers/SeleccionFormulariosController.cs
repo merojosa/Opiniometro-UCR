dbda3d979268e949699ca1de44fdea93e1cd47a7
hernantorres <hernan.torres@ucr.ac.cr>
2019-06-09 23:00:44 -0600

    Pequeño ejercicio para cargar vista parcial con js.

---
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/SeleccionFormulariosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Opiniometro_WebApp.Models;
+
+namespace Opiniometro_WebApp.Controllers
+{
+    public class SeleccionFormulariosController : Controller
+    {
+        private Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
+
+        // GET: SeleccionFormularios
+        public ActionResult SeleccionFormularios()
+        {
+            return PartialView(db.Formulario.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult POSTSeleccionFormularios(string form)
+        {
+            IQueryable<Formulario> formulario = from f in db.Formulario select f;
+
+            if (!String.IsNullOrEmpty(form))
+            {
+                formulario = formulario.Where(f => f.Nombre.Equals(form));
+            }
+
+            return PartialView(formulario);
+        }
+    }
+}
