588f4da31ee4244b6f8673aba31dd9e79e6ef046
JoseAndresMejiasRojas <jose.mejiasrojas@ucr.ac.cr>
2019-06-10 18:00:32 -0600

    Implementado un ejemplo en Home para usar el servicio de permisos.  PruebaHtml es un ejemplo de igual manera.

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/HomeController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/HomeController.cs
@@ -6,18 +6,24 @@ using System.Web.Mvc;
 using Opiniometro_WebApp.Models;
 using System.Security.Claims;
 using System.Threading;
+using Opiniometro_WebApp.Controllers.Servicios;
 
 namespace Opiniometro_WebApp.Controllers
 {
-
-
     [Authorize]
     public class HomeController : Controller
     {
         
         public ActionResult Index()
         {
-            return View("Index");
+            if(IdentidadManager.verificar_sesion(this) == true)
+            {
+                return View("Index");
+            }
+            else
+            {
+                return RedirectToAction("Login", "Auth");
+            }
         }
 
         public ActionResult About()
