a74fbbd09f006960c550744fc56d2a368f0ba4e5
Aldo Weil <aldo.weil@ucr.ac.cr>
2019-06-13 14:10:12 -0600

    Se arreglaron errores de compilacion por cambios en postdeployment

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/AdminController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/AdminController.cs
@@ -80,7 +80,7 @@ namespace Opiniometro_WebApp.Controllers
             {
                 using (db)
                 {
-                    db.SP_ModificarPersona(per.Persona.Cedula, per.Persona.Cedula, per.Persona.Nombre, per.Persona.Apellido1, per.Persona.Apellido2, per.Persona.Direccion);
+                    db.SP_ModificarPersona(per.Persona.Cedula, per.Persona.Cedula, per.Persona.Nombre1, "",per.Persona.Apellido1, per.Persona.Apellido2, per.Persona.DireccionDetallada);
                     return RedirectToAction("VerPersonas");
                 }
             }
