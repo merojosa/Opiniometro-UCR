e82dbf953d087913965ac26b3700ad0692546e92
sebastianVUCR <sebastian.vargas.99@gmail.com>
2019-06-12 20:27:45 -0600

    HUs seleccionar perfil y home
    Se hizo una pantalla con seleccionar perfil
    Se pusieron los permisos en el home
    Se insertaron tuplas para probar permisos
    Se quitó la tupla que daba datos inconsistentes en el proyecto de la base
    La rama del equipo tenía otro string de conexión entonces lo cambié. Eso pudo haber generado cambios en archivos de config
    Modifiqué el modelo de PerfilesUsuario (yo lo hice y creo que nadie más lo usó porque el programa compiló)
    Se agregaron los permisos en el archivo IdPermiso

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/PerfilController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/PerfilController.cs
@@ -14,6 +14,48 @@ namespace Opiniometro_WebApp.Controllers
 {
     public class PerfilController : Controller
     {
+        [Authorize]
+        public ActionResult Index()
+        {
+            Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
+            string correo_autenticado = IdentidadManager.obtener_correo_actual();
+            //ICollection<String> perfiles;
+            //perfiles = db.ObtenerPerfilUsuario(correo_autenticado).ToList();
+            PerfilesUsuario model = new PerfilesUsuario();
+            model.ListaPerfiles = db.ObtenerPerfilUsuario(correo_autenticado).ToList();
+            return View(model);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(PerfilesUsuario model)
+        {
+            String perfil_elegido = model.perfilSeleccionado;
+            // Si es un distinto perfil, cambie los permisos.
+            if (IdentidadManager.obtener_perfil_actual() != perfil_elegido)
+            {
+                string correo_actual = IdentidadManager.obtener_correo_actual();
+
+                var identidad = new ClaimsIdentity(
+                        new[] {
+                    new Claim(ClaimTypes.Email, correo_actual),
+
+                    // Nuevo perfil.
+                    new Claim(ClaimTypes.Role, perfil_elegido)
+                        },
+                        DefaultAuthenticationTypes.ApplicationCookie);
+
+                AuthController.eliminar_privilegios(this);
+
+                Thread.CurrentPrincipal = new ClaimsPrincipal(identidad);
+                HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties { IsPersistent = false }, identidad);
+                Session[correo_actual] = new IdentidadManager();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
         // GET: Perfil
         public ActionResult CambioPerfil(string perfil_elegido)
         {
@@ -45,7 +87,6 @@ namespace Opiniometro_WebApp.Controllers
         public static ICollection<String> ObtenerPerfiles()
         {
             Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
-
             string correo_autenticado = IdentidadManager.obtener_correo_actual();
             ICollection<String> perfiles;
             perfiles = db.ObtenerPerfilUsuario(correo_autenticado).ToList();
