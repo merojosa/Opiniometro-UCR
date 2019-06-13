a76aeda76f9f7aab66de540326835bf775411d2e
KaloDelgado <jean.delgadoramirez@ucr.ac.cr>
2019-06-13 17:32:25 -0600

    Se descomentan los using que daban "ambiguedad"

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/ItemController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/ItemController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Data; //Problema de ambiguedad
+using System.Data; //Problema de ambiguedad
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
