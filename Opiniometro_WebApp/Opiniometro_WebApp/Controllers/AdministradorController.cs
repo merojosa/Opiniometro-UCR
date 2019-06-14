1be324f92e9e91528677a1f4a31d2ee69a7afbf9
Aldo Weil <aldo.weil@ucr.ac.cr>
2019-06-14 00:19:17 -0600

    Se arreglaron errores como consecuencia del merge.

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/AdministradorController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/AdministradorController.cs
@@ -259,8 +259,7 @@ namespace Opiniometro_WebApp.Controllers
         private void InsercionEnBloque(DataTable tablaAInsertar)
         {
             //string hileraConexion = ConfigurationManager.ConnectionStrings["Opiniometro_DatosEntities"].ConnectionString;
-            string hileraConexion =
-                "data source=(localdb)\\ProjectsV13;initial catalog=Opiniometro_Datos;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+            string hileraConexion = "data source=10.1.4.55;initial catalog=Opiniometro;persist security info=True;user id=DeveloperOp;password=devop2019;multipleactiveresultsets=True;application name=EntityFramework&quot;";
             using (SqlConnection conexionBD = new SqlConnection(hileraConexion))
             {
                 
