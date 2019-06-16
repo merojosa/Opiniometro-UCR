65884260764be88ba0a14ca431c53460fe0cf718
Jose Pablo Salas Masís <jose.las@hotmail.com>
2019-06-16 09:59:38 -0600

    Creación del controlador para asignar periodo

---
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/AsignarPeriodoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Opiniometro_WebApp.Models;
+using System.Diagnostics;
+
+namespace Opiniometro_WebApp.Controllers
+{
+    public class AsignarPeriodoController : Controller
+    {
+        private Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
+
+        //prueba para mocking tests
+        public AsignarPeriodoController()
+        {
+            db = new Opiniometro_DatosEntities();
+        }
+
+        public AsignarPeriodoController(Opiniometro_DatosEntities db)
+        {
+            this.db = db;
+        }
+        // GET: AsignarPeriodo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
