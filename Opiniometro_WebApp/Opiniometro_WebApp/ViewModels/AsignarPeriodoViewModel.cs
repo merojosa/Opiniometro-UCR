f3144d384cd4e50f7e7a1f73d0fd7830474ac7b3
Jose Pablo Salas Masís <jose.las@hotmail.com>
2019-06-16 10:19:19 -0600

    View Model para asignar Periodo

---
+++ Opiniometro_WebApp/Opiniometro_WebApp/ViewModels/AsignarPeriodoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Opiniometro_WebApp.Models
+{
+
+    public class AsignarPeriodoViewModel
+    {
+        public IEnumerable<Ciclo_Lectivo> Ciclos { get; set; }
+        public IEnumerable<Carrera> Carreras { get; set; }
+        public IEnumerable<Enfasis> Enfasis { get; set; }
+        public IEnumerable<Curso> Cursos { get; set; }
+        public IEnumerable<Unidad_Academica> UnidadesAcademicas { get; set; }
+
+        // Lista de grupos
+        public IQueryable<ElegirGrupoEditorViewModel> Grupos { get; set; }
+
+
+    }
+}
