1f59661f51437bd02133a17ebbdadc18b3e5ea01
felipealmanza <felipe.almanza@ucr.ac.cr>
2019-06-16 17:20:05 -0600

    Correcciones

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/SeleccionPermisosController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/SeleccionPermisosController.cs
@@ -12,39 +12,26 @@ namespace Opiniometro_WebApp.Controllers
 {
     public class SeleccionPermisosController : Controller
     {
-        private Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
         // GET: SeleccionPermisos
         public ActionResult SeleccionarPermisos()
         {
-            SeleccionPermisos seleccionPermisos = new SeleccionPermisos();
-            SeleccionPermisos model = seleccionPermisos;
-            model.ListaPerfiles = db.Perfil.ToList();
-            model.ListaPermisos = db.Permiso.ToList();
-            model.ListaPosee = db.Posee_Enfasis_Perfil_Permiso.ToList();
-            int tam = model.ListaPerfiles.Count;
-
-            List<String> listaIds = Perfil.ObtenerIds();
-
-            return View(model);
+            using (var context = new Opiniometro_DatosEntities())
+            {
+                SeleccionPermisos seleccionPermisos = new SeleccionPermisos();
+                SeleccionPermisos model = seleccionPermisos;
+                model.ListaPerfiles = context.Perfil.ToList();
+                model.ListaPermisos = context.Permiso.ToList();
+                model.ListaPosee = context.Posee_Enfasis_Perfil_Permiso.ToList();
+                model.ListaPerfilesId = Perfil.ObtenerIds();
+                model.ListaEnfasis = context.Enfasis.ToList();
+                return View(model);
+            }
         }
 
         [HttpPost]
         public ActionResult Guardar()
         {
             return View();
-        }
-
-        private void InsertarPersona(Persona p)
-        {
-            using (var context = new Opiniometro_DatosEntities())
-            {
-                context.Persona.Add(p);
-            }
-            var persona = new Perfil()
-            {
-                Id = "1234567",
-                Tipo = "Felipe"
-            };
         }
     }
 }
