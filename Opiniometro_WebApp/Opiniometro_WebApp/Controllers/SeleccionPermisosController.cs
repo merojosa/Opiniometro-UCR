25383d88f4b48828501c9046adbaf9f824fd293b
felipealmanza <felipe.almanza@ucr.ac.cr>
2019-06-21 01:20:22 -0600

    Avances

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/SeleccionPermisosController.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/SeleccionPermisosController.cs
@@ -17,19 +17,56 @@ namespace Opiniometro_WebApp.Controllers
         // GET: SeleccionPermisos
         public ActionResult SeleccionarPermisos()
         {
-            if(IdentidadManager.verificar_sesion(this) == true)
+            if (IdentidadManager.verificar_sesion(this) == true)
             {
-                if(IdentidadManager.obtener_perfil_actual() == "Admin")
+                if (IdentidadManager.obtener_perfil_actual() == "Admin")
                 {
+                    bool asignado;
                     using (var context = new Opiniometro_DatosEntities())
                     {
-                        SeleccionPermisos seleccionPermisos = new SeleccionPermisos();
-                        SeleccionPermisos model = seleccionPermisos;
-                        model.IdPerfil = "";
+                        SeleccionPermisos model = new SeleccionPermisos();
                         model.ListaPerfiles = context.Perfil.ToList();
                         model.ListaPerfilesId = Perfil.ObtenerIds();
                         model.ListaPermisos = context.Permiso.ToList();
                         model.ListaPosee = context.Posee_Enfasis_Perfil_Permiso.ToList();
+                        model.ListaEnfasis = context.Enfasis.ToList();
+                        model.ListaAsoc = new List<SeleccionPermisos.Asociaciones>();
+                        //model.ListaGuardar = new List<SeleccionPermisos.GuardarPerm>();
+
+                        foreach (var posee in model.ListaPosee)
+                        {
+                            SeleccionPermisos.Asociaciones asoc = new SeleccionPermisos.Asociaciones(posee.IdPerfil, posee.IdPermiso);
+                            //Para verificar si ya existe la combinacion de permiso en una tupla solamente de permiso (id) y perfil (id)
+                            //para no insertarla en la lista ListaAsoc. Todo esto pues hay tuplas muy similares debido a que lo que difiere es el enfasis
+                            if (!model.ListaAsoc.Any(item => item.Perfil == asoc.Perfil && item.Permiso == asoc.Permiso))
+                            {
+                                model.ListaAsoc.Add(asoc);
+                            }
+                        }
+
+                        foreach (var a in model.ListaAsoc)
+                        {
+                            System.Diagnostics.Debug.Print(a.Perfil + " " + a.Permiso);
+                        }
+
+                        foreach (var perfil in model.ListaPerfiles)
+                        {
+                            foreach (var permiso in model.ListaPermisos)
+                            {
+                                asignado = false;
+                                for (int cont = 0; cont < model.ListaAsoc.Count; cont++)
+                                {
+                                    if (perfil.Id == model.ListaAsoc[cont].Perfil)
+                                    {
+                                        if (permiso.Id == model.ListaAsoc[cont].Permiso)
+                                        {
+                                            model.ListaGuardar.Add(new SeleccionPermisos.GuardarPerm(perfil.Id, permiso.Id));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
                         model.ListaEnfasis = context.Enfasis.ToList();
                         return View(model);
                     }
--- Opiniometro_WebApp/Opiniometro_WebApp/Models/SeleccionPermisos.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Models/SeleccionPermisos.cs
@@ -7,6 +7,17 @@ namespace Opiniometro_WebApp.Models
 {
     public class SeleccionPermisos
     {
+        public class Asociaciones
+        {
+            public Asociaciones(string idPerfil, short idPerm)
+            {
+                this.Perfil = idPerfil;
+                this.Permiso = idPerm;
+            }
+
+            public string Perfil { get; set; }
+            public short Permiso { get; set; }
+        }
 
         public class GuardarPerm
         {
@@ -19,22 +30,20 @@ namespace Opiniometro_WebApp.Models
                 this.Perfil = idPerfil;
                 this.Permiso = idPermiso;
             }
-            
+
             public string Perfil { get; set; }
             public short Permiso { get; set; }
             public bool Existe { get; set; }
         }
 
-        public Permiso PermisoModel { get; set; }
-        public Perfil PerfilModel { get; set; }
         //IdPerfil es para la seleccion actual de perfil
         public string IdPerfil { get; set; }
         public List<Enfasis> ListaEnfasis { get; set; }
         public List<Permiso> ListaPermisos { get; set; }
         public List<Perfil> ListaPerfiles { get; set; }
         public List<GuardarPerm> ListaGuardar { get; set; }
-
-        public List<Posee_Enfasis_Perfil_Permiso> ListaPosee {get; set;}
+        public List<Asociaciones> ListaAsoc { get; set; }
+        public List<Posee_Enfasis_Perfil_Permiso> ListaPosee { get; set; }
         public List<String> ListaPerfilesId { get; set; }
     }
 }
