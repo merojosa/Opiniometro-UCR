3fa11783fff40983004854b113f47cccd13c365d
hernan torres <hernan.torres@ucr.ac.cr>
2019-06-21 11:48:59 -0600

    Actividad supervisada 7: primera versión de formulario desplegado.

---
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/LlenarFormularioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Opiniometro_WebApp.Models;
+
+namespace Opiniometro_WebApp.Controllers
+{
+    public class LlenarFormularioController : Controller
+    {
+        private Opiniometro_DatosEntities db = new Opiniometro_DatosEntities();
+        // GET: LlenarFormulario
+        public ActionResult Index()
+        {
+            return View(db.Conformado_Item_Sec_Form.Where(f => f.CodigoFormulario.Contains("121212")));
+        }
+    }
+}
