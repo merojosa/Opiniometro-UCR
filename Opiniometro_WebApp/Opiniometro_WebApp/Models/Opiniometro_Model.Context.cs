a9c3ec07ac5d5dcd0c3323282fd1f55580b4baf2
Javier5614 <javier.nunezaraya@ucr.ac.cr>
2019-06-23 22:16:28 -0600

    Cambios en el modelo para agregar la nueva tabla

--- Opiniometro_WebApp/Opiniometro_WebApp/Models/Opiniometro_Model.Context.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Models/Opiniometro_Model.Context.cs
@@ -62,6 +62,7 @@ namespace Opiniometro_WebApp.Models
         public virtual DbSet<Tiene_Usuario_Perfil_Enfasis> Tiene_Usuario_Perfil_Enfasis { get; set; }
         public virtual DbSet<Unidad_Academica> Unidad_Academica { get; set; }
         public virtual DbSet<Usuario> Usuario { get; set; }
+        public virtual DbSet<Conformado_For_Sec> Conformado_For_Sec { get; set; }
     
         public virtual ObjectResult<CursosSegunAnno_Result> CursosSegunAnno(Nullable<short> anno)
         {
