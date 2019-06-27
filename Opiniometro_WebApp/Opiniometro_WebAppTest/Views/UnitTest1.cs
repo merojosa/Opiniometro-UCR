aff42f9549572f49f327f3dc746580eac99dc83b
fitzberth powell channer <vidar1996@hotmail.com>
2019-06-27 16:59:23 -0600

    re creacion del modelo

---
+++ Opiniometro_WebApp/Opiniometro_WebAppTest/Views/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Opiniometro_WebAppTest.Views
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
