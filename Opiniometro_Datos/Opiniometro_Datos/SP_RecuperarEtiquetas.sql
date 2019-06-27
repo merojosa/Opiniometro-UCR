711a7d352d0b73700f134c02b58871e9d3e61451
fitzberth powell channer <vidar1996@hotmail.com>
2019-06-27 14:31:58 -0600

    primera iteracion SP_RecuperarEtiquetas

---
+++ Opiniometro_Datos/Opiniometro_Datos/SP_RecuperarEtiquetas.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[SP_RecuperarEtiquetas]
+	@tipoPregunta INT,
+	@idPregunta INT
+AS
+BEGIN
+	IF @tipoPregunta = 2 --seleccion unica
+		BEGIN
+			SELECT	OpcionRespuesta
+			FROM	Opciones_De_Respuestas_Seleccion_Unica 
+			WHERE	ItemId = @idPregunta
+		END
+	ELSE IF @tipoPregunta = 3 --like dislike(si/no)
+		BEGIN
+			SELECT	OpcionRespuesta
+			FROM	Opciones_De_Respuestas_Seleccion_Unica
+			WHERE	ItemId = @idPregunta
+		END
+	ELSE IF @tipoPregunta = 4 --seleccion multiple
+		BEGIN
+			SELECT	OpcionRespuesta
+			FROM	Opciones_De_Respuestas_Seleccion_Multiple
+			WHERE	ItemID = @idPregunta
+		END
+	ELSE IF @tipoPregunta = 5 --Escalar
+		BEGIN
+			SELECT	Escalar.Inicio,Escalar.Fin
+			FROM	Escalar 
+			WHERE	ItemId = @idPregunta
+		END
+	ELSE IF @tipoPregunta = 6 --Escalar Estrella
+		BEGIN
+			SELECT	Escalar.Inicio,Escalar.Fin
+			FROM	Escalar 
+			WHERE	ItemId = @idPregunta
+		END
+END
