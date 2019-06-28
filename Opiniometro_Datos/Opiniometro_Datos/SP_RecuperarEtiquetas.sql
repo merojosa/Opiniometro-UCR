a11017cc606a744f14957c0d73bd368ef8f6d589
fitzberth powell channer <vidar1996@hotmail.com>
2019-06-28 09:19:54 -0600

    agregada funcionalidad de transaccion al SP_RecuperarEtiquetas.sql

--- Opiniometro_Datos/Opiniometro_Datos/SP_RecuperarEtiquetas.sql
+++ Opiniometro_Datos/Opiniometro_Datos/SP_RecuperarEtiquetas.sql
@@ -3,34 +3,37 @@
 	@idPregunta INT
 AS
 BEGIN
-	IF @tipoPregunta = 2 --seleccion unica
-		BEGIN
-			SELECT	OpcionRespuesta
-			FROM	Opciones_De_Respuestas_Seleccion_Unica 
-			WHERE	ItemId = @idPregunta
-		END
-	ELSE IF @tipoPregunta = 3 --like dislike(si/no)
-		BEGIN
-			SELECT	OpcionRespuesta
-			FROM	Opciones_De_Respuestas_Seleccion_Unica
-			WHERE	ItemId = @idPregunta
-		END
-	ELSE IF @tipoPregunta = 4 --seleccion multiple
-		BEGIN
-			SELECT	OpcionRespuesta
-			FROM	Opciones_De_Respuestas_Seleccion_Multiple
-			WHERE	ItemID = @idPregunta
-		END
-	ELSE IF @tipoPregunta = 5 --Escalar
-		BEGIN
-			SELECT	Escalar.Inicio,Escalar.Fin
-			FROM	Escalar 
-			WHERE	ItemId = @idPregunta
-		END
-	ELSE IF @tipoPregunta = 6 --Escalar Estrella
-		BEGIN
-			SELECT	Escalar.Inicio,Escalar.Fin
-			FROM	Escalar 
-			WHERE	ItemId = @idPregunta
-		END
+	SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
+	BEGIN TRANSACTION t1
+		IF @tipoPregunta = 2 --seleccion unica
+			BEGIN
+				SELECT	OpcionRespuesta
+				FROM	Opciones_De_Respuestas_Seleccion_Unica 
+				WHERE	ItemId = @idPregunta
+			END
+		ELSE IF @tipoPregunta = 3 --like dislike(si/no)
+			BEGIN
+				SELECT	OpcionRespuesta
+				FROM	Opciones_De_Respuestas_Seleccion_Unica
+				WHERE	ItemId = @idPregunta
+			END
+		ELSE IF @tipoPregunta = 4 --seleccion multiple
+			BEGIN	
+				SELECT	OpcionRespuesta
+				FROM	Opciones_De_Respuestas_Seleccion_Multiple
+				WHERE	ItemID = @idPregunta
+			END
+		ELSE IF @tipoPregunta = 5 --Escalar
+			BEGIN
+				SELECT	Escalar.Inicio,Escalar.Fin
+				FROM	Escalar 
+				WHERE	ItemId = @idPregunta
+			END
+		ELSE IF @tipoPregunta = 6 --Escalar Estrella
+			BEGIN
+				SELECT	Escalar.Inicio,Escalar.Fin
+				FROM	Escalar 
+				WHERE	ItemId = @idPregunta
+			END
+	COMMIT TRANSACTION t1
 END
