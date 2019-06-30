d2262232b5669fd9a7d35c7293834405883a7ac3
Fabian Antonio Rojas Masis <fabian.rojasmasis@ucr.ac.cr>
2019-06-30 14:43:47 -0600

    Modificacion de los modelos para la vista previa de formulario.

--- Opiniometro_WebApp/Opiniometro_WebApp/Models/CrearFormularioModel.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Models/CrearFormularioModel.cs
@@ -23,8 +23,15 @@ namespace Opiniometro_WebApp.Models
         public Nullable<int> Orden_Item { get; set; }
 
         public Nullable<int> Orden_Seccion { get; set; }
+
+        public FormularioCompletoModel FormularioCompleto { get; set; }
     }
-    
+    public class FormularioCompletoModel
+    {
+        public List<Conformado_Item_Sec_Form> Conformados { get; set; }
+
+        public List<Conformado_For_Sec> ConformadoS { get; set; }
+    }
 
 
 }
