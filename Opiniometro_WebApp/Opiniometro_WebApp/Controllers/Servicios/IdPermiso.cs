17775f1d5c1e04983eb8e26fbe495005a44679b3
joseandresmr <jose.mejiasrojas@ucr.ac.cr>
2019-07-05 11:30:36 -0600

    Arreglados dos errores: el rollback de crear perfil y el permiso de asignar formulario.

--- Opiniometro_WebApp/Opiniometro_WebApp/Controllers/Servicios/IdPermiso.cs
+++ Opiniometro_WebApp/Opiniometro_WebApp/Controllers/Servicios/IdPermiso.cs
@@ -30,7 +30,6 @@ namespace Opiniometro_WebApp.Controllers.Servicios
 
         // Ejemplos
         public const int HACER_TODO = 1;
-        public const int ASIGNAR_FORMUALRIO = 2;
         public const int CALIFICAR_CURSOS = 3;
         public const int VER_CURSOS = 4;
         public const int ELIMINAR_USUARIO = 5;
