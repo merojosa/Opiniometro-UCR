c21e98c6f2a4e4b9665f58e517d379be0f1e93f7
Sergio <sergiofv55@gmail.com>
2019-07-10 13:15:49 -0600

    cambio en trigger TR_InsertaPersona

--- Opiniometro_Datos/Opiniometro_Datos/Script.PostDeployment.sql
+++ Opiniometro_Datos/Opiniometro_Datos/Script.PostDeployment.sql
@@ -946,8 +946,7 @@ CREATE TRIGGER TR_InsertaPersona
 ON Persona INSTEAD OF INSERT
 AS
 BEGIN
-	set transaction isolation level serializable;
-	begin transaction tInsertaPersona;
+	SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 	DECLARE @cedula CHAR(10)
 	DECLARE @nombre1 NVARCHAR(51)
 	DECLARE @nombre2 NVARCHAR(51)
@@ -958,21 +957,28 @@ BEGIN
 	SET @nombre2				= (SELECT Nombre2 FROM inserted)
 	SET @apellido1				= (SELECT Apellido1 FROM inserted)
 	SET @apellido2				= (SELECT Apellido2 FROM inserted)
+
 	IF((@cedula NOT LIKE '') AND  (LEN(@cedula) = 9) AND (@nombre1 NOT LIKE '') AND (LEN(@nombre1) <= 50) 
-	AND  (LEN(@nombre2) <= 50)  AND (@apellido1 NOT LIKE '') AND (LEN(@apellido1) <= 50)  
+	AND (@apellido1 NOT LIKE '') AND (LEN(@apellido1) <= 50)  
 	AND (@apellido2 NOT LIKE '') AND (LEN(@apellido2) <= 50))
 	BEGIN
-		INSERT INTO Persona(Cedula, nombre1, nombre2, apellido1, apellido2)
-		VALUES (@cedula, @nombre1, @nombre2, @apellido1, @apellido2)
+		BEGIN TRY
+			BEGIN TRANSACTION tInsertaPersona;
+			INSERT INTO Persona(Cedula, nombre1, nombre2, apellido1, apellido2)
+			VALUES (@cedula, @nombre1, @nombre2, @apellido1, @apellido2)
+			COMMIT TRANSACTION tInsertaPersona;
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRANSACTION tInsertaPersona;
+		END CATCH
 	END
 	ELSE
 	BEGIN
 		RAISERROR('Hay campos no pueden estar vacíos o exceder el tamaño adecuado', 16, 1)
 		RETURN
 	END
-	commit transaction tInsertaPersona;
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 END;
-
 
 IF OBJECT_ID('TR_InsertaUsuario') IS NOT NULL
 	DROP TRIGGER TR_InsertaUsuario
