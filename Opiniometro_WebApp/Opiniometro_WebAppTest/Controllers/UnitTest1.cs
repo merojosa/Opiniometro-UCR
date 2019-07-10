b76319a2b7203657d77b82c74b23b9adb5879614
Sergio <sergiofv55@gmail.com>
2019-07-09 20:12:18 -0600

    test de aministrador empezado a partir del test que habia en admin

---
+++ Opiniometro_WebApp/Opiniometro_WebAppTest/Controllers/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Opiniometro_WebApp.Controllers;
+using Opiniometro_WebApp.Controllers.Servicios;
+using System.Net;
+using Opiniometro_WebApp.Models;
+
+
+namespace Opiniometro_WebAppTest.Controllers
+{
+    [TestClass]
+    public class AdministradorControllerTest
+    {
+        [TestMethod]
+        public void EditIsNotNull()
+        {
+            //Arrange
+            AdministradorController controller = new AdministradorController();
+            //Act
+            ViewResult result = controller.Editar("116720500") as ViewResult;
+            //Assert
+            Assert.IsNotNull(result, "Null");
+        }
+    }
+}
