e48082cfbb8fc23696b05c3452bc2f73e945e335
Oscar Arroyo Garcia <oscar.arroyogarcia@ucr.ac.cr>
2019-07-10 14:30:04 -0600

    Se agrega un SP para Insertar datos en la tabla Selección Única en el proyecto de datos.

---
+++ Opiniometro_Datos/Opiniometro_Datos/SP_Insertar_Seleccion_Unica.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[SP_Insertar_Seleccion_Unica]
+	@itemid NVARCHAR(10),
+	@isalikedislike bit
+AS
+BEGIN 
+	SET IMPLICIT_TRANSACTIONS OFF; 
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+	BEGIN TRANSACTION Insertar_Seleccion_Unica;
+		INSERT INTO Seleccion_Unica( ItemId, IsaLikeDislike )
+		VALUES	( @itemid, @isalikedislike );
+	COMMIT TRANSACTION Insertar_Seleccion_Unica;
+END 
